04b362fe70b10c1fbd5d9988b1b354ca9bf783f1
root <sylyac2000@gmail.com>
2022-09-14 22:00:50 -0700

    DBStorage - States and Cities

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage
+	and DBStorage
+"""
+
+
+from os import getenv
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+# from base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
+if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 
-storage = FileStorage()
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
+
 storage.reload()
