556353b7834b19aaa1784054a4171d0fa4939ab4
root <sylyac2000@gmail.com>
2022-10-02 21:07:29 -0700

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
-# A Bash script that sets up your web servers for the deployment of web_static
+#install nginx and create some Folders
 
+install_nginx(){
+	apt-get  update -y &> /dev/null
+	apt-get  upgrade -y &> /dev/null
+	apt-get  install nginx -y &> /dev/null
+}
+create_folders(){
+	mkdir -p /data/
+	mkdir -p /data/web_static/
+	mkdir -p /data/web_static/releases/
+	mkdir -p /data/web_static/shared/
+	mkdir -p /data/web_static/releases/test/
+}
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello from test home" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '63i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+create_index(){
+	echo "Salam ALX" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+}
+
+create_symbolic(){
+	#create simbolic link(ln-s cible destination_ou_nom)
+	sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+}
+
+change_owner(){
+	#Give ownership of the /data/ folder to ubuntu:ubuntu recursive
+	sudo chown -hRf ubuntu:ubuntu /data/*
+}
+
+update_nginx_default() {
+	#Update the Nginx configuration
+	sudo sed -i '63i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+}
+main() {
+
+	install_nginx
+	create_folders
+	create_index
+	create_symbolic
+	change_owner
+	update_nginx_default
+
+}
+main
+
+sudo service nginx restart
