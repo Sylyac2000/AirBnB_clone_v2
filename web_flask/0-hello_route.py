c2e06e6a1aebcc2915102d116333f5246ec907d5
root <sylyac2000@gmail.com>
2022-10-16 15:42:15 -0700

    Write a script that starts a Flask web application:

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Starts a Flask web application """
+""" Starts Flask application """
 
 from flask import Flask
 app = Flask(__name__)
@@ -13,20 +12,3 @@ def hello_holberton():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-=======
-"""Starts a Flask web application.
-"""
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
->>>>>>> d577dd864bf9e4e11ea89e2298dde622ae347831
